56d50580ab786d5364c11d155b5905627a5ea3d7
jania <janiamsanchez@gmail.com>
2024-08-31 11:17:18 -0600

    Introducción a Microservicios Spring Boot 1/2
    Introducción a Microservicios Spring Boot 1/2

---
+++ springboot-service-celulares/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung','S21', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung','S22', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung','S23', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Apple','Iphone 14', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Apple','Iphone 15', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Xiaomi','Mi', NOW());
