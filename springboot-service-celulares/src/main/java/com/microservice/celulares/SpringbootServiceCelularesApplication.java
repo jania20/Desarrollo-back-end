0c8578c8134eec32fb7e302ea6252b2c45f9d1ca
jania <janiamsanchez@gmail.com>
2024-09-25 12:03:51 -0600

    Netflix Eureka Client
    se agregó dependencias en "springboot service-store" y también se modificaron unos detallitos que tenia

--- springboot-service-celulares/src/main/java/com/microservice/celulares/SpringbootServiceCelularesApplication.java
+++ springboot-service-celulares/src/main/java/com/microservice/celulares/SpringbootServiceCelularesApplication.java
@@ -1,9 +1,12 @@
 package com.microservice.celulares;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringbootServiceCelularesApplication {
 
 	public static void main(String[] args) {
