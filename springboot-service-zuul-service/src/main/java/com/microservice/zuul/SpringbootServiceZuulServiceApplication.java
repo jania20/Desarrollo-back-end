fff3ed2f75d8853442fcd99ff23e5021c670dda6
jania <janiamsanchez@gmail.com>
2024-10-12 21:28:50 -0600

    idk
    idk

---
+++ springboot-service-zuul-service/src/main/java/com/microservice/zuul/SpringbootServiceZuulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiceZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceZuulServiceApplication.class, args);
+	}
+
+}
